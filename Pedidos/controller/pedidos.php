faa2db6e459720e718f5a47c835f2cc0af3ffa59
juancruzunah <jacruzr@unah.hn>
2021-11-20 21:26:31 -0600

    Actualizar la ubicacion del carpetas

--- Pedidos/controller/pedidos.php
+++ Pedidos/controller/pedidos.php
@@ -11,8 +11,8 @@
   header('Access-Control-Allow-Origin: *');
   header('Content-Type:application/json');
 
-  require_once ("../config/conexion.php");
-  require_once ("../models/Pedidos.php");
+  require_once ("../../config/conexion.php");
+  require_once ("../../Pedidos/models/Pedidos.php");
 
   $pedidos=new Pedidos();
 
